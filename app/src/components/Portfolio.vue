d2382182c244691dcd43d283ce156107be21e6bb
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-04-07 17:27:51 +0100

    feat: imagens novas adicionadas

--- app/src/components/Portfolio.vue
+++ app/src/components/Portfolio.vue
@@ -1,16 +1,34 @@
+<script setup>
+import sistemastudio from '@/assets/img/portfolio/sistemastudio.png';
+import mifelviagens from '@/assets/img/portfolio/mifelviagens.png';
+import pryanick from '@/assets/img/portfolio/pryanick.png';
+import sistemaaposta from '@/assets/img/portfolio/sistemaaposta.png';
+import diagnosticocaes from '@/assets/img/portfolio/diagnosticocaes.png';
+import johnsabores from '@/assets/img/portfolio/johnsabores.png';
+</script>
+
 <template>
    <section id="portfolio" class="py-5 bg-dark">
     <div class="container">
       <h2 class="section-title text-center">Portfólio</h2>
       <div class="row g-4">
         <div class="col-md-4" data-aos="fade-up">
-          <img src="https://images.unsplash.com/photo-1460925895917-afdab827c52f" class="img-fluid rounded-3 shadow" alt="Projeto 1">
+          <img :src="sistemastudio" class="img-fluid rounded-3 shadow" alt="Projeto 1">
         </div>
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-          <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085" class="img-fluid rounded-3 shadow" alt="Projeto 2">
+          <img :src="mifelviagens" class="img-fluid rounded-3 shadow" alt="Projeto 2">
         </div>
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
-          <img src="https://images.unsplash.com/photo-1551281044-8b1c1f5c2f0c" class="img-fluid rounded-3 shadow" alt="Projeto 3">
+          <img :src="pryanick" class="img-fluid rounded-3 shadow" alt="Projeto 3">
+        </div>
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+          <img :src="diagnosticocaes" class="img-fluid rounded-3 shadow" alt="Projeto 3">
+        </div>
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+          <img :src="sistemaaposta" class="img-fluid rounded-3 shadow" alt="Projeto 3">
+        </div>
+        <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+          <img :src="johnsabores" class="img-fluid rounded-3 shadow" alt="Projeto 3">
         </div>
       </div>
       <div class="text-center mt-4">
