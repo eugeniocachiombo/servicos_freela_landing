7574d4a295f4ab9efa34353f221d78a66aa5c846
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-05-06 11:30:25 +0100

    test: test

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="hero text-center" id="hero">
     <div class="container" data-aos="fade-up">
-      <img src="/src/assets/img/profile.jpg" alt="Eugénio Cachiombo" class="mx-auto mb-4" />
+      <img src="src/assets/img/profile.jpg" alt="Eugénio Cachiombo" class="mx-auto mb-4" />
       <h1 class="display-4 fw-bold mb-3">Eugénio Cachiombo</h1>
       <p class="lead highlight fs-3 mb-2">Desenvolvedor de Software</p>
       <p class="col-lg-8 mx-auto fs-5 mb-4">
