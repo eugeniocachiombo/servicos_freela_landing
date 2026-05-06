2b30e262852a5db081901d98d3726afc49d264b9
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-05-07 00:27:14 +0100

    perf: comment

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/css/main.css'
 import router from './router'
+import './assets/bootstrap-5.0.2/css/bootstrap.css';
+import './assets/bootstrap-5.0.2/js/bootstrap.js';
+import './assets/fontawesome/css/all6.css';
+/*import './assets/aos/aos.css';
+import './assets/aos/aos.js';*/
+import './assets/css/main.css'
+
+
+
 
 createApp(App).use(router).mount('#app')
